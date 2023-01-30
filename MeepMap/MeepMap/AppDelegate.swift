607757a8ac82e5d86d89203955b1d0cd28433af8
Guido Fabio <gui2_fabio@hotmail.com>
2023-01-30 19:45:57 +0100

    protecting apiKey

--- MeepMap/MeepMap/AppDelegate.swift
+++ MeepMap/MeepMap/AppDelegate.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Override point for customization after application launch.\\
+        var keys: NSDictionary?
+        
+        if let path = Bundle.main.path(forResource: "keys", ofType: "plist") {
+            keys = NSDictionary(contentsOfFile: path)
+        }
+        
+        if let dict = keys, let apiKey = dict["googleMapKey"] as? String {
+            GMSServices.provideAPIKey(apiKey)
+        }
         return true
     }
 
@@ -30,7 +38,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
 
 }
 
